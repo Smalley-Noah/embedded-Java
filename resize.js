c2f4d51174c2460117632d0308e7ea44a4b119fb
Metlegen <7nsmalley@stpatrick.org>
2017-03-22 14:00:52 -0500

    Add files via upload

--- resize.js
+++ resize.js
@@ -1,4 +1,16 @@
-//Declare tabletSize() here
-
-
-//Declare phoneSize() here
+//tabletSize
+function samecolor(){
+    let color = "#154965";
+}
+//phoneSize
+function diffcolor(){
+    let color = "#157834";
+}
+//color
+function tabletSize(){
+    let color = "#546821"
+}
+//diffcolor
+function phonesize(){
+    let color = "#264884"
+}
